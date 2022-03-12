4d481d77b333daa1ed97d07080ba8aec39b40ad0
Ray <raiyan.ahmed@gmail.com>
2022-03-13 00:57:33 +1100

    Recovered from missing osrm files.

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default { 
     port: 1755,
-    port_osrm: 5000,
+    PORT_OSRM: 5000,
     PATH_IMG: './src/public/building-ids-',
     PATH_JSON: './src/public/buildings.json',
     IMAGE_COL_LEN: 6, // The number of images that will be present.
---
+++ src/service/osrm.service.ts
@@ -0,0 +1,31 @@
+
+import { changeBuildingsObject } from '../controller/handleBuildingData.controller';
+import http from 'http';
+import config from 'config';
+import { Building } from '../interface/building.interface';
+import { TransportMode } from '../interface/distanceMatrix.interface';
+import axios from 'axios';
+
+const getDistanceMatrix = (geojson: any, deserializedData: any, objectKey: string, addressString: string, 
+                           buildingFrom?: Building, buildingTo?: Building, transportType?: TransportMode): any => {
+    let matrixJson = ''
+    let port_osrm = config.get<string>('PORT_OSRM');
+  
+    let isochroneString = buildingFrom?.longitude + ',' + buildingFrom?.latitude + ';' +
+                          buildingTo?.longitude + ',' + buildingTo?.latitude;
+    
+    let dummy = '151.2153,-33.8568;151.1780,-33.7961'
+    let URL = 'http://localhost:'+ port_osrm + '/route/v1/foot/' + isochroneString;
+
+    axios.get(URL).then(res => {
+        matrixJson = res.data;
+        changeBuildingsObject(geojson, deserializedData, objectKey, addressString);
+        console.log(matrixJson);
+    });
+    
+
+    return matrixJson;
+}
+
+
+export default getDistanceMatrix;
